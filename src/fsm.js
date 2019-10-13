fc549cfedbd87a4db49d083dfd3ae8fe4f05330d
Irina Stepanova <stira_04@mail.ru>
2019-10-13 08:58:09 +0600

    feat: add finite-state-machine-task solution

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,62 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        // задаем начальные значения
+        this.statesArray = config.states;
+        this.activeState = config.initial;
+        this.initialState = config.initial;
+		this.getStatesArray = [];
+		this.actionsArray = [];
+		this.redoActionsArray = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        // возвращаем текущее значение состояния
+        return this.activeState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        // изменяем состояние на заданное, либо возвращаем ошибку, если заданного состояния не существует
+        if (this.statesArray[state]) {
+            this.actionsArray.push(this.activeState);
+            this.activeState = state;
+            this.redoActionsArray = [];
+        }
+        else {
+            throw new Error;
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        // если событие существует, переходим в соответствующее ему состояние, иначе - выдаем ошибку
+        if (this.statesArray[this.activeState].transitions[event]) {
+            this.changeState(this.statesArray[this.activeState].transitions[event]);
+        }
+		else {
+            throw new Error;
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        // возвращаемся в изначальное состояние
+        this.activeState = this.initialState;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +66,67 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        // если событие не задано, возвращаем все возможные состояния
+        if (event === undefined) {
+			return Object.keys(this.statesArray);
+		}
+		else {
+            // если событие задано, возвращаем все соответствующие ему состояния
+            let getStatesByEvent = [];
+            Object.keys(this.statesArray).forEach(state => {
+                if (this.statesArray[state].transitions[event]) {
+                    getStatesByEvent.push(state);
+                }
+            })
+            return getStatesByEvent;
+        }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        // если массив с пройденными состояниями пуст - отменять нечего - возвращаем false
+        if (this.actionsArray.length == 0) {
+			return false;
+		}
+		else {
+            // если не пуст - записываем последнее состояние в redoActionsArray и возвращаемся к предыдущему
+            this.redoActionsArray.push(this.activeState);
+            this.activeState = this.actionsArray.pop();
+            return true;
+		}
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        // если массив с отмененными состояниями пуст - возвращать нечего
+        if (this.redoActionsArray.length == 0) {
+			return false;
+		}
+		else {
+            // если не пуст - записываем последнее состояние в actionsArray и шагаем на состояние вперед
+            this.actionsArray.push(this.activeState);
+            this.activeState = this.redoActionsArray.pop();
+            return true;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        // очищаем историю состояний
+        this.actionsArray = [];
+		this.redoActionsArray = [];
+    }
 }
 
 module.exports = FSM;
